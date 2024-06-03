d5105ea0f64612e352efdf2983c429d8c7b635b8
tdam <20456596+tobiasdam@users.noreply.github.com>
2024-06-03 14:03:00 +0200

    format code and added env vars

--- redis_queue_listener.py
+++ redis_queue_listener.py
@@ -1,22 +1,27 @@
 import redis
 import threading
 
+
 class RedisQueueListener:
-    def __init__(self, queue_name, callback):
+    def __init__(self, queue_name, redis_conn, callback):
         self.queue_name = queue_name
         self.callback = callback
-        self.redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+        self.redis_conn = redis.Redis(
+            host=redis_conn["host"], port=redis_conn["port"], db=redis_conn["db"]
+        )
         self.pubsub = self.redis_conn.pubsub()
         self.pubsub.subscribe(self.queue_name)
         self.thread = threading.Thread(target=self.listen)
-        self.thread.daemon = True  # Set the thread as a daemon to exit when the main program exits
-    
+        self.thread.daemon = (
+            True  # Set the thread as a daemon to exit when the main program exits
+        )
+
     def listen(self):
         for message in self.pubsub.listen():
-            #print(message)
-            if message['type'] == 'message':
-                command = message['data']
-                if command == b'lpush':
+            # print(message)
+            if message["type"] == "message":
+                command = message["data"]
+                if command == b"lpush":
                     data = self.redis_conn.rpop("lbcommands")
                     self.callback(data)
 
--- template_loader.py
+++ template_loader.py
@@ -7,16 +7,21 @@ import nodered_id_gen
 import LBflow
 from error_messages import error_messages
 from io import BytesIO
-
+import os
 
 # TODO: replace eth to localhost later on bridge
 
-nodered_address = "10.203.14.242:1880"
+NODERED_HOST = os.environ.get("NODERED_HOST", "localhost")
+NODERED_PORT = int(os.environ.get("NODERED_PORT", 1880))
+
 nodered_mqtt_broker = ""
 
-redis_address = '10.203.14.242'
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
+REDIS_DB = int(os.environ.get("REDIS_DB", 0))
 
-def load_nodered_template(template_filename : str) -> dict:
+
+def load_nodered_template(template_filename: str) -> dict:
     nr_template_dict = {}
 
     # feature req: add sanity check for json?
@@ -36,13 +41,23 @@ def load_nodered_template(template_filename : str) -> dict:
             nr_old_template = nr_old_template.replace(id_to_be_replaced, new_id)
 
         return json.loads(nr_old_template)
-    
-def compose_nodered_flow_to_json(flow : LBflow, output_file : str) -> int:
+
+
+def compose_nodered_flow_to_json(flow: LBflow, output_file: str) -> int:
     nodered_flow = []
 
     fetch_nodered_mqtt_broker()
 
-    header = {"id": nodered_id_gen.generate_nodered_id(), "type":"tab", "label":"lorabridge flow", "disabled": True, "info": "True heros drink matcha.", "env": [], "nodes": [], "configs": []}
+    header = {
+        "id": nodered_id_gen.generate_nodered_id(),
+        "type": "tab",
+        "label": "lorabridge flow",
+        "disabled": True,
+        "info": "True heros drink matcha.",
+        "env": [],
+        "nodes": [],
+        "configs": [],
+    }
 
     nodered_flow.append(header)
 
@@ -51,26 +66,39 @@ def compose_nodered_flow_to_json(flow : LBflow, output_file : str) -> int:
         for template_nodes in node.nodered_template[1:]:
             template_nodes["z"] = header["id"]
 
-            if template_nodes["type"] == "mqtt in" or template_nodes["type"] == "mqtt out":
+            if (
+                template_nodes["type"] == "mqtt in"
+                or template_nodes["type"] == "mqtt out"
+            ):
                 template_nodes["broker"] = nodered_mqtt_broker
 
-            if template_nodes["type"] == "mqtt in" and node.nodered_template[0]["type"] == "lb_mqtt_input":
-                template_nodes["topic"] = 'zigbee2mqtt/'+get_device_ieee_id(node.device_id)
+            if (
+                template_nodes["type"] == "mqtt in"
+                and node.nodered_template[0]["type"] == "lb_mqtt_input"
+            ):
+                template_nodes["topic"] = "zigbee2mqtt/" + get_device_ieee_id(
+                    node.device_id
+                )
 
             # TODO: mqtt output topic should actually be defined in the template and not hardcoded here.
 
-            if template_nodes["type"] == "mqtt out" and node.nodered_template[0]["type"] == "lb_mqtt_output":
-                template_nodes["topic"] = 'zigbee2mqtt/'+get_device_ieee_id(node.device_id)+'/set'
+            if (
+                template_nodes["type"] == "mqtt out"
+                and node.nodered_template[0]["type"] == "lb_mqtt_output"
+            ):
+                template_nodes["topic"] = (
+                    "zigbee2mqtt/" + get_device_ieee_id(node.device_id) + "/set"
+                )
 
             nodered_flow[0]["nodes"].append(template_nodes)
 
-        #nodered_flow.append(node.nodered_template[1:])
+        # nodered_flow.append(node.nodered_template[1:])
 
     # Check for parameters to be updated/initialized
 
     if "parameters" in node.nodered_template[0]:
         for parameter in node.nodered_template[0]["parameters"]:
-            
+
             parameter_node_id = parameter["node_id"]
             parameter_tag = parameter["nametag"]
             parameter_nodekey = parameter["nodekey"]
@@ -82,52 +110,50 @@ def compose_nodered_flow_to_json(flow : LBflow, output_file : str) -> int:
                     # If parametertag -is- the value, then we substitute the value itself
 
                     # TODO: sanity check for type violations (corrupted template)
-                    
+
                     if type(nodered_node[parameter_nodekey]) == str:
                         if nodered_node[parameter_nodekey] == parameter_tag:
                             nodered_node[parameter_nodekey] = parameter_value
-                        else:                            
-                            nodered_node[parameter_nodekey] = nodered_node[parameter_nodekey].replace(parameter_tag, str(parameter_value))
-                    
-                        
+                        else:
+                            nodered_node[parameter_nodekey] = nodered_node[
+                                parameter_nodekey
+                            ].replace(parameter_tag, str(parameter_value))
 
     nodered_flow_json = json.dumps(nodered_flow[0], indent=4)
 
-    with open(output_file,"w") as json_file:
+    with open(output_file, "w") as json_file:
         json_file.write(nodered_flow_json)
 
     return 0
 
+
 def fetch_nodered_mqtt_broker() -> int:
-    
+
     global nodered_mqtt_broker
 
     buffer = BytesIO()
     c = pycurl.Curl()
-    c.setopt(c.URL, nodered_address+"/flow/global")
+    c.setopt(c.URL, ":".join((NODERED_HOST, NODERED_PORT)) + "/flow/global")
     c.setopt(c.WRITEDATA, buffer)
     c.perform()
     c.close()
 
     body = buffer.getvalue()
-    body = body.decode('utf-8')
+    body = body.decode("utf-8")
     response_json = json.loads(body)
 
     for config in response_json["configs"]:
         if config["type"] == "mqtt-broker":
-            nodered_mqtt_broker = config["id"]            
-            print("MQTT broker found, id: "+nodered_mqtt_broker)
+            nodered_mqtt_broker = config["id"]
+            print("MQTT broker found, id: " + nodered_mqtt_broker)
             return error_messages.NO_ERRORS
-   
 
     return error_messages.MQTT_BROKER_NOT_FOUND
 
+
 def get_device_ieee_id(lb_id):
+    redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
 
-    global redis_address
+    value = redis_client.hget("lorabridge:device:registry:id", lb_id)
 
-    redis_client = redis.Redis(host=redis_address, port=6379, db=0)
-
-    value = redis_client.hget('lorabridge:device:registry:id', lb_id)
-
-    return value.decode('utf-8')
+    return value.decode("utf-8")
