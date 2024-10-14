3a1d54db90a4987b113277b2588be1525c8136f6
tdam <20456596+tobiasdam@users.noreply.github.com>
2024-10-14 12:06:10 +0200

    empty queue on startup

--- redis_queue_listener.py
+++ redis_queue_listener.py
@@ -10,16 +10,19 @@ class RedisQueueListener(threading.Thread):
         self.redis_conn = redis.Redis(
             host=redis_conn["host"], port=redis_conn["port"], db=redis_conn["db"]
         )
-       
+
         self.pubsub = self.redis_conn.pubsub()
         self.pubsub.subscribe(self.queue_name)
-        self.daemon=True
+        self.daemon = True
         # self.thread = threading.Thread(target=self.listen)
         # self.thread.daemon = (
         #     True  # Set the thread as a daemon to exit when the main program exits
         # )
 
-    def run(self):           
+    def run(self):
+        while item := self.redis_conn.rpop():
+            self.callback(item)
+
         for message in self.pubsub.listen():
             print(message)
             if message["type"] == "message":
@@ -28,7 +31,6 @@ class RedisQueueListener(threading.Thread):
                     data = self.redis_conn.rpop("lbcommands")
                     if data is not None:
                         self.callback(data)
-       
 
     # def listen(self):
     #     for message in self.pubsub.listen():
