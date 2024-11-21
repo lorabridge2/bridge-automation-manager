d1fc174e4fabe7d7daca95b3ed1b64e557be89e8
tdam <20456596+tobiasdam@users.noreply.github.com>
2024-11-21 16:34:26 +0100

    change restored flows from bein [[flow,flow,flow]] to [flow,flow,flow]

--- decompression.py
+++ decompression.py
@@ -177,7 +177,7 @@ def restore_flow(flow_filename) -> LBflow | None:
 
 def restore_flows():
     for flow_file in glob.glob("backup/*.dat"):
-        flows.append(restore_flow(flow_file))
+        flows.extend(restore_flow(flow_file))
 
 
 def seek_flow(_flow_id) -> LBflow:
